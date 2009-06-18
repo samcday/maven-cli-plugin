fb6b08bed426053ed051eac5d3dafbea5a4ab2dd
shaolang <shaolang@ymail.com>
2009-06-18 14:20:18 +0800

    Replace the need to create Embedder, as Maven and PlexusContainer can be also be retrieved from MavenSession.

--- maven/src/main/java/org/twdata/maven/cli/CommandCallRunner.java
+++ maven/src/main/java/org/twdata/maven/cli/CommandCallRunner.java
@@ -10,17 +10,12 @@ import org.apache.maven.plugin.logging.Log;
 import org.apache.maven.profiles.DefaultProfileManager;
 import org.apache.maven.profiles.ProfileManager;
 import org.apache.maven.project.MavenProject;
-import org.codehaus.plexus.PlexusContainerException;
-import org.codehaus.plexus.component.repository.exception.ComponentLookupException;
-import org.codehaus.plexus.embed.Embedder;
 
 class CommandCallRunner {
     private final MavenSession session;
     private final MavenProject project;
     private final Log logger;
-    private Embedder embedder;
-    private Maven embeddedMaven;
-    private File userDir;
+    private final File userDir;
     private boolean pluginExecutionOfflineMode;
 
     public CommandCallRunner(MavenSession session, MavenProject project,
@@ -28,26 +23,8 @@ class CommandCallRunner {
         this.session = session;
         this.project = project;
         this.logger = logger;
-
-        initEmbeddedMaven();
-        resolvePluginExecutionOfflineMode();
-    }
-
-    private void initEmbeddedMaven() throws MojoExecutionException {
-        try {
-            embedder = new Embedder();
-            embedder.start();
-            embeddedMaven = (Maven) embedder.lookup(Maven.ROLE);
-            userDir = new File(System.getProperty("user.dir"));
-        } catch (PlexusContainerException e) {
-            throw new MojoExecutionException(e.getMessage());
-        } catch (ComponentLookupException e) {
-            throw new MojoExecutionException(e.getMessage());
-        }
-    }
-
-    private void resolvePluginExecutionOfflineMode() {
-        pluginExecutionOfflineMode = session.getSettings().isOffline();
+        this.userDir = new File(System.getProperty("user.dir"));
+        this.pluginExecutionOfflineMode = session.getSettings().isOffline();
     }
 
     public void executeCommand(CommandCall commandCall) {
@@ -59,7 +36,7 @@ class CommandCallRunner {
 
                 session.setCurrentProject(currentProject);
                 session.getSettings().setOffline(commandCall.isOffline() ? true : pluginExecutionOfflineMode);
-                ProfileManager profileManager = new DefaultProfileManager(embedder.getContainer(),
+                ProfileManager profileManager = new DefaultProfileManager(session.getContainer(),
                         commandCall.getProperties());
                 profileManager.explicitlyActivate(commandCall.getProfiles());
                 MavenExecutionRequest request = new DefaultMavenExecutionRequest(
@@ -73,7 +50,7 @@ class CommandCallRunner {
                 }
                 request.setPomFile(new File(currentProject.getBasedir(),
                         "pom.xml").getPath());
-                embeddedMaven.execute(request);
+                ((Maven) session.lookup(Maven.ROLE)).execute(request);
                 logger.info("Current project: " + project.getArtifactId());
             } catch (Exception e) {
                 logger.error(

c84681f54d9aeb1ea77ca75006a2492b5754f862
shaolang <shaolang@ymail.com>
2009-06-22 11:17:28 +0800

    Removed unused imports in CommandCallBuilder.

--- maven/src/main/java/org/twdata/maven/cli/CommandCallBuilder.java
+++ maven/src/main/java/org/twdata/maven/cli/CommandCallBuilder.java
@@ -1,8 +1,6 @@
 package org.twdata.maven.cli;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.regex.Pattern;
